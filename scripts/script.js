883a3f67e61a4ee35995839f87fadd1373e9c502
Rafael Uribe <rafagallardo75@gmail.com>
2022-09-28 11:25:56 -0500

    new image sizing

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,9 @@
 const heroDiv = document.getElementById("heroSlider")
 import heroComponent from "./components/heroComponent.js";
 
-const heroImage1URL = "./assets/images/4.png"
-const heroImage2URL = "./assets/images/5.png"
-const heroImage3URL = "./assets/images/6.png"
+const heroImage1URL = "./assets/images/1.png"
+const heroImage2URL = "./assets/images/2.png"
+const heroImage3URL = "./assets/images/3.png"
 const heroP1 = "Frase 1"
 const heroP2 = "Frase 2"
 const heroP3 = "Frase 3"
