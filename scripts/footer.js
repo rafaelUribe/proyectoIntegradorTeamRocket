f8f155a99a256a38761254db8e118005f8aca28b
Luis Gonzalez <tm.pasadoypresente@gmail.com>
2022-10-25 12:38:02 -0500

    previo a la presentacion

---
+++ scripts/footer.js
@@ -0,0 +1,43 @@
+const footer = () => `
+<footer class="footer">
+        <div class="grupo-1">
+            <div class="box">
+                <!-- Logo -->
+                <figure>
+                    <a href="./index.html">
+                        <img src="../assets/images/flor-guiedani-white.png" alt="Logo Guiedani">
+                    </a>
+                </figure>
+            </div>
+            <!-- Links navbar -->
+            <div class="box">
+                <a href="./index.html">Home</a><br>
+                <a href="./catalogue.html">Productos</a><br>
+                <a href="./acerca.html">Nosotros</a><br>
+                <a href="./">Contacto</a><br>
+                <!-- <a href="#">Blog</a><br> -->
+                <a href="#">Términos y condiciones</a><br>
+                <a href="#">Políticas de privacidad</a>
+            </div>
+            <!-- Redes sociales -->
+            <div class="box">
+                <h2>Síguenos</h2>
+                <div class="red-social">
+                    <a href="https://www.facebook.com/" target="_blank">
+                        <img src="https://i.ibb.co/56c8NPm/facebook-icon-1.png" alt="logo facebook">
+                    </a>
+                    <a href="https://www.instagram.com/" target="_blank">
+                        <img src="https://i.ibb.co/rpkRmNh/instagram-icon.png" alt="logo instagram">
+                    </a>
+                    <a href="https://twitter.com/?lang=es" target="_blank">
+                        <img src="https://i.ibb.co/1d0pgHX/twitter-icon.png" alt="logo instagram">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="grupo-2">
+            <small>&copy; 2022 <strong>Guiedani</strong> Todos los derechos reservados</small>
+        </div>
+    </footer>`
+
+    export default footer
