97f36d2e924bd6047aea6262fb0e4297585ff370
Leslie <leslie.gonzalez1799@gmail.com>
2022-10-25 13:37:08 -0500

    import js carrito y footer

--- scripts/components/pagina-producto.js
+++ scripts/components/pagina-producto.js
@@ -1,7 +1,16 @@
 /* Cambiar imagen pequeña a imagen principal de producto */
+import countItemsInCart from "../utilities/countItemsInCart.js";
+import footer from "./footer.js";
 
 const mainImg = document.getElementById("mainImg");
 const smallImg = document.getElementsByClassName("small-img");
+let producto = {};
+
+const getProductFromLocalStorage = () => {
+    producto = JSON.parse(localStorage.getItem("detail"))
+    producto = JSON.parse(producto)
+    console.log(producto)
+ }
 
 smallImg[0].onclick = () =>{
     mainImg.src = smallImg[0].src;
@@ -15,3 +24,7 @@ smallImg[2].onclick = () =>{
 smallImg[3].onclick = () =>{
     mainImg.src = smallImg[3].src;
 };
+
+getProductFromLocalStorage();
+footer();
+countItemsInCart();
