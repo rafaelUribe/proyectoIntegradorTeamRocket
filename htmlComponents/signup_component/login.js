fe9532154216e252b94c5a3aec02e3ec5f6b53a0
Luis Gonzalez <tm.pasadoypresente@gmail.com>
2022-10-28 08:51:40 -0500

    login completo: lee usuarios de base de datos y manda todos los mensajes pertinentes

--- htmlComponents/signup_component/login.js
+++ htmlComponents/signup_component/login.js
@@ -1,33 +1,16 @@
-/* baseDeDatos = localStorage.getItem("sistema-de-login");
-
-if (!baseDeDatos) {
-    cargarDatosUsuario();
-}
-
-function cargarDatosUsuario () {
-    badeDeDatos = {
-        "Luis": {
-            password : "Ponciano"
-        },
-        "Ethan": {
-            password : "HumanoProgramado"
-        }
-    }
-} */
 //  ==================== Creamos base de datos de usuarios ====================
 const baseUsuarios = {
-    "Luis": {
-        password : "Ponciano"
-    },
-    "Ethan": {
-        password : "Humano"
-    }
+    "Luis": "123",
+    "Ethan" : "Humano", 
+    "Maria" : "321"
+    
 }
-
 
 // ==================== La guardamos en el local storage ====================
 
 localStorage.setItem('usuarios', JSON.stringify(baseUsuarios));
+//usuarios es el nombre que yo le doy en el localStorage
+//baseUsuarios es el nombre de mi objeto
 
 //==================== Recupoeramos base de datos del localstorage ====================
 const usuarios = JSON.parse(localStorage.getItem('usuarios'));
@@ -36,18 +19,19 @@ console.log(usuarios);
 //==================== Boton de acceder ====================
 
 
-function login(){
-    
+function login() {
 
 
+    //Guardamos los elementos que el usuario ponga como usuario y password
+    //User y pass son los id que pusimos en el form
     let usuario = document.getElementById("user").value;
     let contraseña = document.getElementById("pass").value;
 
-    warning ="";
+    warning = "";
     if (!usuario && !contraseña) {
-            warning += `<br> Ingresa usuario y contraseña <br> `
-            parrafo.innerHTML = warning;
-            return false
+        warning += `<br> Ingresa usuario y contraseña <br> `
+        parrafo.innerHTML = warning;
+        return false
     }
 
     if (!usuario) {
@@ -61,32 +45,37 @@ function login(){
         parrafo.innerHTML = warning;
         return false
     }
+//==================== Aquí buscamos los usuatrios ====================
 
+//En el for externo iteramos a los usuarios
+    for (let buscarUsuario in usuarios) {
+        console.log("Aqui imprimimos los usuarios pa ver si los carga");
+        console.log(buscarUsuario);
+        console.log(usuarios[buscarUsuario]);
 
-
-        //Aquí buscamos el usuario
-
-        //const keysUsuarios = Object.keys(baseUsuarios).map(key => {
-            //console.log(key);
-            if (usuario == "Luis" && contraseña == 123) {
-
-        
-        
-
-                //console.log(baseDeDatos)
-        
+        //En este ciclo comparamos el usuario de entrada con los usuarios
+        //Si encuentra al usuario entra a este ciclo
+        if (usuario == buscarUsuario ) {//Aqui compara la contraseña de entrada con la del usuario en la base
+            if (contraseña == usuarios[buscarUsuario]) {
+                //Si ls dos coniciden se logea correctamente
                 window.location = "https://www.youtube.com/watch?v=NwzGmP_BJaE&ab_channel=JudithTiral";
-        
-            } 
 
+            } else {
+                //Si la contraseña no coincide manda mensaje
+                warning += `<br> Contraseña incorrecta <br>`
+               parrafo.innerHTML = warning;
+               return false
+            }
+    
+        } 
 
-
-
-
-
-    else {
-        warning += `<br> Usuario no registrado <br>`
-        parrafo.innerHTML = warning;
-        return false
     }
+
+    //Cuando no encuentra al usuario sale del ciclo for in e imprimimos este mensaje
+
+    warning += `<br> Usuario no registrado <br>`
+    parrafo.innerHTML = warning;
+
+
+
 }
