3626f879c3d63e58c7b38a7c7b023ab2b6117c9d
mariaReveles <113363673+mariaReveles@users.noreply.github.com>
2022-11-09 11:19:30 -0600

    funcion mandar correo

--- scripts/contacto.js
+++ scripts/contacto.js
@@ -2,6 +2,27 @@ console.log("Funcion footer() conectada")
 import countItemsInCart from "./utilities/countItemsInCart.js"
 import footer from "./components/footer.js"
 
+  function sendMail(){
+    var params = {
+        name: document.getElementById("validationServerName").value,
+        email: document.getElementById("validationServerEmail").value,
+        message: document.getElementById("validationServerText").value,
+    };
+  const serviceID  ="service_zz5zy7b";
+  const templateID = "template_n2lqmkn";
+
+  emailjs
+    .send(serviceID, templateID, params)
+    .then((res) => {
+            document.getElementById("validationServerName").value = "";
+            document.getElementById("validationServerEmail").value = "";
+            document.getElementById("validationServerText").value = "";
+            console.log(res);
+            alert("tu mensaje se mandó correctamente");
+         })
+         .catch(err=>console.log(err));
+}
+
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
@@ -22,7 +43,6 @@ import footer from "./components/footer.js"
       }, false)
     })
   })()
-
 
 
   
